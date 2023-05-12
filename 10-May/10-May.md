65b1eb8ebd4032f50eb07ec27b0de0e78e7b1770
Charlo-tech <charliewachira124@gmail.com>
2023-05-12 11:05:44 +0300

    may 10 answer

--- 10-May/10-May.md
+++ 10-May/10-May.md
@@ -1,8 +1,8 @@
 ### May 10th Answer
 
-## Minimum cost: 503
+## Minimum cost: 475
 
-Using python, we found the minimum cost of the 39 workers to be <b>503</b> after excluding the worker with the highest efficiency rating from the array.
+Using python, we found the minimum cost of the 39 workers to be <b>475</b> after iterating through possible pairs in the array.
 
-Manually, we sorted the efficiencies in ascending order and paired adjacent numbers then found the sum of the difference between adjacent pairs.
+
 ![Screenshot (60)](https://github.com/Charlo-tech/May-DSA-AH/assets/57678615/b383c7ef-e437-4ccc-9648-add0409c06f4)
--- 10-May/efficiency.py
+++ 10-May/efficiency.py
@@ -1,12 +1,14 @@
-efficiencies = [1, 3, 54, 712, 52, 904, 113, 12, 135, 32, 31, 56, 23, 79, 611, 123, 677, 232, 997, 101, 111, 123, 2, 7, 24, 57, 99, 45, 666, 42, 104, 129, 554, 335, 876, 35, 15, 93, 13]
-
-# Arrange the numbers in ascending order
-efficiencies.sort()
-
-# Pair the first 38 numbers
-pairs = [(efficiencies[i], efficiencies[i+1]) for i in range(0, 38, 2)]
-
-# Find the sum of the differences between the pairs
-minimum_cost = sum(abs(pair[0] - pair[1]) for pair in pairs)
-
-print("The Minimum cost of given efficiencies is:", minimum_cost)
+def min_task_cost(efficiency):
+    min_cost = float('inf')
+    n = len(efficiency)
+    for i in range(n):
+        # exclude worker i and form pairs with remaining workers
+        remaining = efficiency[:i] + efficiency[i+1:]
+        remaining.sort()
+        cost = sum(abs(remaining[j] - remaining[j+1]) for j in range(0, n-2, 2))
+        if cost < min_cost:
+            min_cost = cost
+    return min_cost
+efficiency = [1, 3, 54, 712, 52, 904, 113, 12, 135, 32, 31, 56, 23, 79, 611, 123, 677, 232, 997, 101, 111, 123, 2, 7, 24, 57, 99, 45, 666, 42, 104, 129, 554, 335, 876, 35, 15, 93, 13]
+min_cost = min_task_cost(efficiency)
+print("The Minimum cost is",min_cost)
